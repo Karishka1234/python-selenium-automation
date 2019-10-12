a9f64ea8087d7136fd96f4f9dab7654d3e4a4c5c
Karishka1234 <sarukhanova.karine@gmail.com>
2019-10-12 18:05:45 -0400

    Homework2

---
+++ locators.py
@@ -0,0 +1,11 @@
+"""
+1. amazon logo field, search by class "a-icon a-icon-logo"
+2. email field, search by ID, “ap_email”
+3. Continue button, search by id='continue', by class 'a-button-input'
+4. Need help link, search by class 'a-expander-prompt'
+5. Forgot your password link, search by id 'auth-fpp-link-bottom'
+6. Other issues with Sign-In link, search by id 'ap-other-signin-issues-link'
+7. Create your Amazon account button, search by id 'createAccountSubmit', also by class 'a-button-text', by role 'button'
+8. Conditions of use link, search by
+
+"""
